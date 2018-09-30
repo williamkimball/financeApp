a23098c38e6e681814c22adebc0a5045a27a3578
William Kimball <wkkimball043@gmail.com>
2018-09-30 18:42:50 -0500

    initial commit

---
+++ src/auth/auth0-variables.js
@@ -0,0 +1,7 @@
+export const AUTH_CONFIG = {
+    clientId: 'dlS2kOZPTdoHH9t1nNEf3VuRXqOw2K3A',
+    domain: 'dartboard.auth0.com',
+    callbackUrl: 'http://localhost:3000/callback',
+    apiUrl: 'YOUR_API_IDENTIFIER'
+  }
+  
